9136b17ce18712ec8a057fc0aead26cbf388a516
MANJUNATH MANAGUTTI <amalabaddur@gmail.com>
2026-03-30 11:02:40 +0530

    Initial commit

---
+++ table.js
@@ -0,0 +1,5 @@
+// take a number from for loop and print the table of that number
+var num = prompt("enter a number to print its table");
+for (var i = 1; i <= 10; i++) {
+    console.log(num + " x " + i + " = " + num * i);
+}
